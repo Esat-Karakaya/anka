8bec620ae3c4ab98895edb99bd9d265cfd399d2a
Esat-Karakaya <esat1karakaya@gmail.com>
2024-11-25 15:15:53 +0300

    removed unused code

--- dilbilgisi/content-connect.js
+++ dilbilgisi/content-connect.js
@@ -1,30 +1,11 @@
-import { replaceForeign } from "./dilbilgisi.js";
 import { textForeignInfo } from "./text-info.js";
 
 chrome.runtime.onMessage.addListener(
     (request, _sender, sendResponse) => {
         if (request.type!=="dilbilgisi") return;
 
-        switch (request.function) {
-            case "getAlternatives":
-                sendResponse(getAlternatives(request.input));
-                break;    
-            case "textForeignInfo":
-                sendResponse(textForeignInfo(request.input));
+        if (request.function === "textForeignInfo") {
+            sendResponse(textForeignInfo(request.input));
         }
     }
 );
-
-//input -> obj[foreignRoot] = [...foreign originals]
-//output -> obj[foreignOriginal] = local replacement
-function getAlternatives(foreignOriginals) {
-    const alternatives = {};
-
-    const toBeFixed=Object.values(foreignOriginals).flat(1);
-
-    for (const word of toBeFixed) {
-        alternatives[word] = replaceForeign(word);
-    }
-    
-    return alternatives;
-}
--- site-scripts/shared.js
+++ site-scripts/shared.js
@@ -1,8 +1,3 @@
-async function getAlternatives(obj) {
-    const res = await chrome.runtime.sendMessage({type: "dilbilgisi", function: "getAlternatives", input:obj});
-    return res;    
-};
-
 async function textForeignInfo(paragraph) {
     const res = await chrome.runtime.sendMessage({type: "dilbilgisi", function: "textForeignInfo", input:paragraph});
     return res;    
